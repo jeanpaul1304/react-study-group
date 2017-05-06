94d800d61b2dce1547f0bf69e24a48cea866528b
jeanpaul1304 <jeanpaul1304>
2017-05-06 11:25:14 -0500

    create a stateless component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Hello = (props) => {
+  return (
+    <h2>Hola {props.name}</h2>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hola Mundo</h2>
+        <Hello name="Jean"></Hello>
+        <Hello name="Paul"></Hello>
+        <Hello name="Eduardo"></Hello>
+        <Hello name="July"></Hello>
       </div>
     );
   }
